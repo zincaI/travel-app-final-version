5cbf5a70f5ff229c1554392e0c465cec9709f1c9
zincaI <zinca.irina13@gmail.com>
2025-01-26 19:46:06 +0200

    Repair commit issues

--- src/app/capital-letters.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CapitalLettersPipe } from './capital-letters.pipe';
-
-describe('CapitalLettersPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CapitalLettersPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/toast-type.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToastTypeDirective } from './toast-type.directive';
-
-describe('ToastTypeDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new ToastTypeDirective();
-    // expect(directive).toBeTruthy();
-  });
-});
